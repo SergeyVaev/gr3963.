c20c5e8f7e95c2c6870604f8fd055c47521feeaf
Сергей Ваев <99madagascar@mail.ru>
2023-02-16 21:18:03 +0300

    выполнил домашнее задание второго семинара

---
+++ Seminar2Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число:");
+string Num = Console.ReadLine();
+if (Num.Length >= 3)
+{
+char[]arr = Num.ToCharArray();
+ Console.WriteLine (arr[2]);
+}
+else
+{
+    Console.WriteLine("третьего числа нет");
+}
---
+++ Seminar2Task9/Program.cs
@@ -0,0 +1,18 @@
+﻿
+System.Random numSintezator = new System.Random();
+
+
+//Вариант 1
+int rndNumber = numSintezator.Next(10,100);
+Console.WriteLine(rndNumber);
+int firstNum = rndNumber/10;
+int secondNum = rndNumber%10;
+if(firstNum>secondNum)
+{
+Console.WriteLine("Первое число больше второго"+firstNum+secondNum);
+}
+else
+{
+Console.WriteLine(secondNum);
+
+}

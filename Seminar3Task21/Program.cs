565d5753504350c575cf063bbfc31f1f94ceba4f
Сергей Ваев <99madagascar@mail.ru>
2023-02-23 09:29:24 +0300

    дз 3 семинар

---
+++ Seminar3Task21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 21
+// Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int ReadData (string msg)// считываем данные
+{
+Console.WriteLine(msg);
+return int.Parse(Console.ReadLine()??"0");
+}
+void PrintData (string msg)//выводим сообщение
+{
+    Console.WriteLine(msg);
+}
+double CalkLen( int x1, int y1, int x2, int y2, int z1, int z2)// вносим переменные для точек
+{
+return Math.Sqrt ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));// выполняем вычисления и возвращаем результат
+}
+int x1 = ReadData("Введите координату х точки А:");//инструкция для пользователя
+int y1 = ReadData ("Введите координату у точки А:");
+int x2 = ReadData("Введите координату z точки A:");
+int y2 = ReadData ("Введите координату х точки В:");
+int z1 = ReadData ("Введите координату y точки B:");
+int z2 = ReadData ("Введите координату z точки B:");
+
+double res =CalkLen(x1, x2, y1, y2, z1, z2) ;
+
+PrintData($"Растояние между точками А({x1},{y1},{z1}) и точкой B({x2},{y2},{z2}) = {res}");// выполнили расчет и отправили
---
+++ Seminar3Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int ReadData (string msg)//считываем данные
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+string LineBuilder(int end, int pow)
+{
+    string res = String.Empty;// пустая строка
+    for(int i = 1; i<=end ;i++)// счетчик,длительность,действия которые выполняем с переменной
+    {
+        res=res+Math.Pow(i,pow)+"\t";
+    }
+    return res;
+}
+int N = ReadData("Введите число N:");//команда для пользователя
+Console.WriteLine(LineBuilder(N,1));//вывод данных
+Console.WriteLine(LineBuilder(N,3));
+

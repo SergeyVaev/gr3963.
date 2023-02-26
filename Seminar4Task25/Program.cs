1e674189a272dc7e366219b0a46755e63b15b152
Сергей Ваев <99madagascar@mail.ru>
2023-02-26 13:49:46 +0300

    выполнил дз к семинару номер 4

---
+++ Seminar4Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 25: Напишите цикл, который принимает
+ //на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+ int ReadData (string msg)//считываем данные. 
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+long Pow (int a, int b)// добавили тип,метод и переменные.
+{
+long res = 1;
+while (b>0) 
+{
+res = res*a ;
+b=b-1;
+}
+return res;//возвращаем полученные значения
+}
+int num1= ReadData("Введите число 1 :");
+int num2=ReadData("Введите число2:");
+Console.WriteLine(Pow(num1,num2));
+
+
---
+++ Seminar4Task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит 
+//их на экран. Ввести с клавиатуры длину массива и диапазон значений элементов.
+
+int ReadData (string msg)//Ввод числа с экрана
+{
+    Console.WriteLine(msg);//выводим приглашение ко вводу
+    return int.Parse(Console.ReadLine()??"0");//приводим к числу
+}
+//Метод для получения случайных значений массива
+int[] GenArr(int num, int min, int max)
+{
+    Random rnd = new Random();
+    int[] arr = new int[num];// объявляем массив
+    for (int i = 0; i < arr.Length; i++)//
+    {
+       arr[i]= rnd.Next(min,max+1);//заполняем случайными числами из диапазона start до end
+    }  
+    return arr;//возвращаем значения в массив
+}
+void PrintArr(int[] arr)
+{
+    Console.Write("[");// указываем с чего будет начинаться вывод на экран
+    for (int i = 0; i<arr.Length-1; i++)//счетчик,условия,действия которые мы будем выполнять
+    {
+        Console.Write(arr[i]+",");//выводим результат через запятую 
+    }
+    Console.WriteLine(arr[arr.Length-1]+"]");//заканчиваем вывод результата крайним числом и закрываем скобкой
+}
+int Length = ReadData("Укажите диапазон длинны массива");
+int min = ReadData("Минимальное значение");
+int max = ReadData("Максимальное значение");
+int[] arr = GenArr( Length, min, max);//заполнение массива случайными числами 
+PrintArr(arr);// вывод результата

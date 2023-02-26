ae5158ef4f884df581a48eaf8b7e4136247f61a9
Сергей Ваев <99madagascar@mail.ru>
2023-02-26 14:15:20 +0300

    отредактировал ошибки

--- Seminar4Task29/Program.cs
+++ Seminar4Task29/Program.cs
@@ -7,17 +7,17 @@ int ReadData (string msg)//Ввод числа с экрана
     return int.Parse(Console.ReadLine()??"0");//приводим к числу
 }
 //Метод для получения случайных значений массива
-int[] GenArr(int num, int min, int max)
+int[] GenArr(int len, int minValue, int maxValue)
 {
     Random rnd = new Random();
-    int[] arr = new int[num];// объявляем массив
+    int[] arr = new int[len];// объявляем массив
     for (int i = 0; i < arr.Length; i++)//
     {
-       arr[i]= rnd.Next(min,max+1);//заполняем случайными числами из диапазона start до end
+       arr[i]= rnd.Next(minValue,maxValue+1);//заполняем случайными числами из диапазона start до end
     }  
     return arr;//возвращаем значения в массив
 }
-void PrintArr(int[] arr)
+void Print1DArr(int[] arr)
 {
     Console.Write("[");// указываем с чего будет начинаться вывод на экран
     for (int i = 0; i<arr.Length-1; i++)//счетчик,условия,действия которые мы будем выполнять
@@ -27,7 +27,7 @@ void PrintArr(int[] arr)
     Console.WriteLine(arr[arr.Length-1]+"]");//заканчиваем вывод результата крайним числом и закрываем скобкой
 }
 int Length = ReadData("Укажите диапазон длинны массива");
-int min = ReadData("Минимальное значение");
-int max = ReadData("Максимальное значение");
-int[] arr = GenArr( Length, min, max);//заполнение массива случайными числами 
-PrintArr(arr);// вывод результата
+int minValue = ReadData("Минимальное значение");
+int maxValue = ReadData("Максимальное значение");
+int[] arr = GenArr( Length, minValue, maxValue);//заполнение массива случайными числами 
+Print1DArr(arr);// вывод результата

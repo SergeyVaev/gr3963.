8133fba5ab39153042bb2ce8ba13bcc7c8228b22
Сергей Ваев <99madagascar@mail.ru>
2023-03-06 19:35:31 +0300

    дз 6 сем

---
+++ Seminar6Task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// //Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int ReadData(string msg)//принимаем число из консоли
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+void PrintData (string msg, int num)//метод выводящий на печать данные пользователю
+{
+    Console.WriteLine(msg+num);
+}
+int CountNum(int numN)//меиод определяющий положительные числа
+{
+    int res=0;
+    for (int i=0; i<numN; i++)
+    {
+        if(ReadData ("")>0)
+        {
+            res++;
+        }
+    }
+return res;
+}
+int numbN=ReadData("Введите числа: ");
+PrintData("Положительных чисел:",CountNum(numbN));
